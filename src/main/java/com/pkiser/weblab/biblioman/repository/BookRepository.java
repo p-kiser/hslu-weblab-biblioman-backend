7ee9834d088191c778b3609ad08509fa33554a9f
Pascal Kiser <pascal.kiser@ajila.com>
2020-03-04 12:58:01 +0100

    tried something

--- src/main/java/com/pkiser/weblab/biblioman/repository/BookRepository.java
+++ src/main/java/com/pkiser/weblab/biblioman/repository/BookRepository.java
@@ -19,5 +19,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @Transactional
 @CrossOrigin(origins = "http://localhost:4200")
 public interface BookRepository extends MongoRepository<BookNode, String> {
-    
+
 }
